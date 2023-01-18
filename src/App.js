c2166ba197d9b2bb980deabf90ddb6190ca583f5
lab <skyebr@yandex.ru>
2023-01-18 11:34:26 +0300

    route & buttons check

--- src/App.js
+++ src/App.js
@@ -3,10 +3,47 @@ import axios from 'axios';
 
 function App() {
 
-      const adminCreateToy = "http://uni-team-inc.online:8000/admin/create"
-      const allToysView = "http://uni-team-inc.online:8000/see?archive=true"  // true - не видна, false - видна
-      const archiveToy = "http://uni-team-inc.online:8000/admin/archive/1"  // Архивируем игрушка
-      const oneToy = "http://uni-team-inc.online:8000/see/1"
+      const adminCreateToy = "http://uni-team-inc.online:8000/admin/create"   // Создать игрушку(title, description, price)
+      const allToysView = "http://uni-team-inc.online:8000/see?archive="      // true - не видна, false - видна
+      const archiveToy = "http://uni-team-inc.online:8000/admin/archive/1"    // Архивируем игрушка
+      const oneToy = "http://uni-team-inc.online:8000/see/1"                  // Посмотреть 1 игрушку по id
+
+
+
+      const adminCreateHandler = async(url) => {
+        await api.post(url,
+            JSON.stringify({
+              title: "",
+              description:"",
+              price:""
+            })
+          )
+            .then(data => {
+              console.log(data.data)
+            })
+        }
+
+
+          const allToysHandler = async(url) => {
+            await api.get(`${url + false}`)
+            .then(data => {
+              console.log(data.data)
+            })
+          }
+
+
+          const archiveToyHandler = async(url, toyId) => {
+            await api.get(`${url + toyId}`)
+          }
+
+          
+          const getOneToyHandler = async(url, toyID) => {
+            await api.get(`${url + toyID}`)
+            .then(data => {
+              console.log(data.data)
+            })
+          }
+
 
       const handlerReg = async() => {
           await axios.post('http://uni-team-inc.online:8080/register', 
@@ -29,6 +66,11 @@ function App() {
     <div className="App">
         <button onClick={() => handlerReg()}>Регистрация</button>
         <button onClick={() => handlerKnock()}>Постучаться</button>
+
+        <button onClick={() => adminCreateHandler(adminCreateToy)}>Создать игрушку</button>
+        <button onClick={() => allToysHandler(allToysView)}>Вывести все игрушки</button>
+        <button onClick={() => archiveToyHandler(archiveToy, 1)}>Архивировать игрушку по id</button>
+        <button onClick={() => getOneToyHandler(oneToy, 1)}>Получить игрушку по id</button>
     </div>
   );
 }
